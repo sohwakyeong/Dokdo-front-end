77c25e1aaef1354f9904285812f9217dd785ba27
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-17 21:11:14 +0900

    Feat : 페이지 추가 및 간단한 퍼블리싱

---
+++ src/pages/literacytest/LiteracyTestMain.Style.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const container = styled.div`
+    text-align: center;
+`;
+
+export const menu = styled.div`
+    display: flex;
+    justify-content: space-around;
+    padding: 15px;
+    background-color: #f3f3f3;
+`;
+
+export const menuItem = styled.a`
+    text-decoration: none;
+    color: #333;
+    cursor: pointer;
+    font-weight: bold;
+`;
+
+export const image = styled.img`
+    width: 100%;
+    max-width: 600px;
+    margin: 20px auto;
+`;
+
+export const startButton = styled.button`
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+`;
+
+export const socialIcons = styled.div`
+    display: flex;
+    justify-content: center;
+    margin: 10px;
+`;
+
+export const socialIcon = styled.a`
+    margin: 0 10px;
+`;
--- src/pages/literacytest/LiteracyTestMain.tsx
+++ src/pages/literacytest/LiteracyTestMain.tsx
@@ -1,1 +1,34 @@
-export {};
+import React from 'react';
+import {
+    container as Container,
+    menu as Menu,
+    menuItem as MenuItem,
+    image as Image,
+    startButton as StartButton,
+    socialIcons as SocialIcons,
+    socialIcon as SocialIcon,
+} from './LiteracyTestMain.Style';
+
+const LiteracyTestMain: React.FC = () => {
+    return (
+        <Container>
+            <Menu>
+                <MenuItem href="#literacy-test">문해력 테스트</MenuItem>
+                <MenuItem href="#ox-quiz">OX 퀴즈</MenuItem>
+            </Menu>
+            <Image src="/" alt="Description" />
+            <StartButton>테스트 시작하기</StartButton>
+            <SocialIcons>
+                <SocialIcon href="https://facebook.com">
+                    <img src="facebook-icon.png" alt="Facebook" />
+                </SocialIcon>
+                <SocialIcon href="https://twitter.com">
+                    <img src="twitter-icon.png" alt="Twitter" />
+                </SocialIcon>
+                {/* 여기에 더 많은 소셜 아이콘 추가 가능 */}
+            </SocialIcons>
+        </Container>
+    );
+};
+
+export default LiteracyTestMain;
