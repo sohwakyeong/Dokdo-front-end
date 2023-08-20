1a262f18f924210cda8cd1cf64bc321f4e7e5bfd
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-21 04:47:59 +0900

    Add : 문제 진행률 프로세스바 추가

--- src/pages/literacytest/LiteracyTestScreen.Style.ts
+++ src/pages/literacytest/LiteracyTestScreen.Style.ts
@@ -1,54 +1,66 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    width: 474px;
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 474px;
 `;
 
 export const QuestionNumber = styled.div`
-    font-weight: bold;
-    margin: 10px;
+  font-weight: bold;
+  margin: 10px;
 `;
 
 export const QuestionText = styled.div`
-    font-size: 20px;
-    margin: 20px;
+  font-size: 20px;
+  margin: 20px;
 `;
 
 export const Options = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 300px;
+  display: flex;
+  flex-direction: column;
+  width: 300px;
 `;
 
 export const OptionButton = styled.button`
-    padding: 10px;
-    margin: 5px;
-    cursor: pointer;
+  padding: 10px;
+  margin: 5px;
+  cursor: pointer;
 `;
 
 export const CurrentPosition = styled.div`
-    font-size: 16px;
-    margin: 10px;
+  font-size: 16px;
+  margin: 10px;
 `;
 export const Navigation = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    width: 300px; // Options와 동일한 너비로 설정
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 300px; // Options와 동일한 너비로 설정
 `;
 
 export const BackButton = styled.button`
-    padding: 10px;
-    cursor: pointer;
-    background-color: #f0f0f0;
-    border: 1px solid #ccc;
-    &:disabled {
-        cursor: not-allowed;
-        opacity: 0.5;
-    }
+  padding: 10px;
+  cursor: pointer;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  &:disabled {
+    cursor: not-allowed;
+    opacity: 0.5;
+  }
+`;
+export const ProgressBarContainer = styled.div`
+  width: 300px;
+  height: 10px;
+  background-color: #f0f0f0;
+  margin: 10px 0;
+`;
+
+export const ProgressBarFiller = styled.div<{ progress: number }>`
+  width: ${props => props.progress}%;
+  height: 100%;
+  background-color: #59c4fc;
 `;
