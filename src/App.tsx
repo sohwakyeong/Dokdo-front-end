46b71d629377e350220305e0ed5fafb217349f5b
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-21 03:14:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import AdminRoutes from './routes/AdminRoutes';
-
-
+import MypageRoutes from './routes/Mypage';
+import Layout1 from './components/layout/layout1/Layout1';
 
 function App() {
   return (
-
-    <Router>      
+    <Router>
       <GlobalStyles />
-      <AdminRoutes />
+      <Layout1>
+      <MypageRoutes />
+      </Layout1>
     </Router>
   );
 }
