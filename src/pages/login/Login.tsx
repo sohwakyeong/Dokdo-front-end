74dc36928a94b0cac7d2502f9315e91c697eef5f
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-21 03:13:48 +0000

    Update Login.tsx

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,71 +1,48 @@
-// import React, { useState } from 'react';
-import {  } from 'react-router-dom';
-import LogoIcon from '../../assets/icon/samplelogo.jpeg';
+import React, { } from 'react';
+import { } from 'react-redux';
+import { } from 'react-cookie';
 
-import * as L from './Login.styled';
+import LogoIcon from '../../assets/icon/samplelogo.jpeg';
+import * as LoginStyle from './Login.styled';
 
 function LoginComponent() {
-  //   const [Email, SetEmail] = useState("");
-  //   const [Password, SetPassword] = useState("");
+
+return (
+<LoginStyle.Container>
+<LoginStyle.LogoImg src={LogoIcon} alt="logo" />
+<LoginStyle.Wrapper>
+<LoginStyle.Title>Login</LoginStyle.Title>
+<LoginStyle.JoinLink to="/user/signup">회원가입하기</LoginStyle.JoinLink>
+</LoginStyle.Wrapper>
+<LoginStyle.Form>
+<LoginStyle.Input
+type="email"
+id="id"
+placeholder="아이디를 입력해주세요."
 
 
-  // const onEmailHandler = (e) => {
-  //   SetEmail(e.target.value);
-  // }
-  // const onPasswordHandler = (e) => {
-  //   SetPassword(e.target.value);
-  // }
-  // const onSubmitHandler = (e) => {
-  //   e.preventDefault();
+/>
+<LoginStyle.Input
+type="password"
 
-  //   console.log('Email', Email);
-  //   console.log('Password', Password);
-
-  //   let formData= {
-  //     email: Email,
-  //     password: Password,
-  //   }
-  //   onSubmit(formData);
-
-  // }
-
-  return (
-    <L.Container>
-      {/* <L.LogoImg src={LogoIcon} alt="logo" />
-      <L.Wrapper>
-        <L.Title>Login</L.Title>
-        <L.JoinLink to="/">회원가입하기</L.JoinLink>
-      </L.Wrapper>
-      <L.Form>
-        <L.Input
-          type="email"
-          id="id"
-          placeholder="아이디를 입력해주세요."
-          value={Email}
-          onChange={onEmailHandler}
-        />
-        <L.Input
-          type="password"
-          value={Password}
-          onChange={onPasswordHandler}
-          placeholder="비밀번호를 입력해주세요."
-        />
-        <L.Save htmlFor="check1">
-          <L.SaveId type="checkbox" id="check1" />
-          아이디 저장하기
-        </L.Save>
-        <L.Button type="submit" onClick={onSubmitHandler}>
-          로그인
-        </L.Button>
-        <L.Button>
-          <L.Kakao to="">카카오톡으로 시작</L.Kakao>
-        </L.Button>
-        <L.Button>
-          <L.Google to="">구글아이디로 시작</L.Google>
-        </L.Button>
-      </L.Form> */}
-    </L.Container>
-  );
+placeholder="비밀번호를 입력해주세요."
+/>
+<LoginStyle.Save htmlFor="check1">
+<LoginStyle.SaveId type="checkbox" id="check1" />
+아이디 저장하기
+</LoginStyle.Save>
+<LoginStyle.Button type="submit">
+로그인
+</LoginStyle.Button>
+<LoginStyle.Button>
+<LoginStyle.Kakao to="">카카오톡으로 시작</LoginStyle.Kakao>
+</LoginStyle.Button>
+<LoginStyle.Button>
+<LoginStyle.Google to="">구글아이디로 시작</LoginStyle.Google>
+</LoginStyle.Button>
+</LoginStyle.Form>
+</LoginStyle.Container>
+);
 }
 
 export default LoginComponent;
