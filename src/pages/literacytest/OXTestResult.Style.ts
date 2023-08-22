759b024df32bb2a2286e9d3f45b4108ef942a4f8
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-23 03:31:34 +0900

    Add : ox퀴즈 또한 개수에 따른 이미지 출력 함수 구현

--- src/pages/literacytest/OXTestResult.Style.ts
+++ src/pages/literacytest/OXTestResult.Style.ts
@@ -1,32 +1,38 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    text-align: center;
-    display: flex;
-    flex-direction: column;
-    align-items: center; // 수평 중앙 정렬
-    justify-content: center; // 수직 중앙 정렬
-    width: 474px;
-    height: 100vh; // 화면의 높이만큼 설정
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center; // 수평 중앙 정렬
+  justify-content: center; // 수직 중앙 정렬
+  width: 474px;
+  height: 100vh; // 화면의 높이만큼 설정
 `;
 
 export const ResultText = styled.div`
-    font-size: 24px; // 원하는 글꼴 크기로 설정
-    font-weight: bold; // 원하는 글꼴 두께로 설정
-    color: #333; // 원하는 색상으로 설정
-    margin: 20px; // 원하는 마진으로 설정
+  font-size: 24px; // 원하는 글꼴 크기로 설정
+  font-weight: bold; // 원하는 글꼴 두께로 설정
+  color: #333; // 원하는 색상으로 설정
+  margin: 20px; // 원하는 마진으로 설정
 `;
 export const SocialIcons = styled.div`
-    display: flex;
-    justify-content: center;
-    margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
 `;
 
 export const SocialIcon = styled.a`
-    margin: 0 10px;
+  margin: 0 10px;
 `;
 
 export const SocialIconImage = styled.img`
-    width: 30px; // 원하는 크기로 설정
-    height: 30px; // 원하는 크기로 설정
+  width: 30px; // 원하는 크기로 설정
+  height: 30px; // 원하는 크기로 설정
+`;
+
+export const ResultImage = styled.img`
+  width: 200px; // 너비 조정
+  height: auto; // 비율 유지
+  margin: 20px 0; // 여백 추가
 `;
--- src/pages/literacytest/OXTestResult.tsx
+++ src/pages/literacytest/OXTestResult.tsx
@@ -5,11 +5,15 @@ import {
   SocialIcons,
   SocialIcon,
   SocialIconImage,
+  ResultImage, // 추가된 스타일 컴포넌트
 } from './OXTestResult.Style';
 import questionsData from '../../assets/data/ox-questions.json';
 import facebookIcon from '../../assets/img/facebook.png';
 import twitterIcon from '../../assets/img/twitter.png';
 import kakaoIcon from '../../assets/img/카카오톡아이콘.png';
+
+import pigImage from '../../assets/img/pig-image.jpeg';
+import smartImage from '../../assets/img/smart-image.jpeg';
 
 type OXTestResultProps = {
   userAnswers: string[]; // 사용자의 답
@@ -23,8 +27,17 @@ const OXTestResult: React.FC<OXTestResultProps> = ({ userAnswers }) => {
     0,
   );
 
+  const getImageSource = () => {
+    if (correctAnswers <= 3) return pigImage;
+    if (correctAnswers <= 6) return smartImage;
+    // 다른 조건에 따른 이미지 반환
+  };
+
+  const imageSource = getImageSource();
+
   return (
     <Container>
+      <ResultImage src={imageSource} alt="Result" /> {/* 이미지 출력 */}
       <ResultText>{`정답 수: ${correctAnswers} / ${questionsData.questions.length}`}</ResultText>
       <SocialIcons>
         <SocialIcon href="https://facebook.com">
@@ -36,7 +49,6 @@ const OXTestResult: React.FC<OXTestResultProps> = ({ userAnswers }) => {
         <SocialIcon href="https://www.kakaocorp.com">
           <SocialIconImage src={kakaoIcon} alt="Kakao" />
         </SocialIcon>
-        {/* 여기에 더 많은 소셜 아이콘 추가 가능 */}
       </SocialIcons>
     </Container>
   );
