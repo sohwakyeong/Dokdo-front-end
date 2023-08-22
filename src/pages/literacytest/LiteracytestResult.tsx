9844b0dae53a5afb4f973b4bd4e8288befb1c4fe
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-23 03:19:39 +0900

    Add : 문해력테스트 부분 정답 개수에 따른 다른 이미지 출력 구현

--- src/pages/literacytest/LiteracytestResult.tsx
+++ src/pages/literacytest/LiteracytestResult.tsx
@@ -6,10 +6,17 @@ import {
   SocialIcons,
   SocialIcon,
   SocialIconImage,
+  ResultImage,
 } from './LiteracytestResult.Style';
 import facebookIcon from '../../assets/img/facebook.png';
 import twitterIcon from '../../assets/img/twitter.png';
 import kakaoIcon from '../../assets/img/카카오톡아이콘.png';
+
+import pigImage from '../../assets/img/pig-image.jpeg';
+import smartImage from '../../assets/img/smart-image.jpeg';
+import geniusImage from '../../assets/img/genius-image.jpeg';
+//import godImage from '../../assets/img/god-image.png';
+//import defaultImage from '../../assets/img/default-image.png';
 
 type LiteracyTestResultProps = {
   userAnswers: number[]; // 사용자가 선택한 답변 인덱스 배열
@@ -30,13 +37,24 @@ const LiteracyTestResult: React.FC<LiteracyTestResultProps> = ({
     if (correctAnswers <= 6) return '꽤나 똑똑합니다.';
     if (correctAnswers <= 9) return 'iq 146 입니다.';
     if (correctAnswers === 10) return '신 입니다';
-    return ''; // 다른 경우에 대한 처리 (필요하다면)
+    return '';
   };
+
+  const getImageSource = () => {
+    if (correctAnswers <= 3) return pigImage;
+    if (correctAnswers <= 6) return smartImage;
+    //if (correctAnswers <= 9) return geniusImage;
+    //if (correctAnswers === 10) return godImage;
+    // return defaultImage;
+  };
+
+  const imageSource = getImageSource();
 
   return (
     <Container>
+      <ResultImage src={imageSource} alt="Result" />
       <ResultText>{`정답 수: ${correctAnswers} / ${questions.questions.length}`}</ResultText>
-      <ResultText>{getResultMessage()}</ResultText> {/* 결과 메시지 출력 */}
+      <ResultText>{getResultMessage()}</ResultText>
       <SocialIcons>
         <SocialIcon href="https://facebook.com">
           <SocialIconImage src={facebookIcon} alt="Facebook" />
