cb8febddcbcbe008f1c4e898f5a0db79d09f8a5f
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-23 14:51:15 +0900

    Fix : 이동경로 수정

--- src/components/layout/footer/Footer.tsx
+++ src/components/layout/footer/Footer.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as FT from './Footer.styled'; 
+import * as FT from './Footer.styled';
 import homeIcon from '../../../assets/icon/Home.png';
 import groupIcon from '../../../assets/icon/Person.png';
 import boardIcon from '../../../assets/icon/Plus.png';
@@ -14,7 +14,7 @@ function Footer() {
           <img src={homeIcon} alt="홈 아이콘" />
           <div>홈</div>
         </FT.StyledLink>
-        <FT.StyledLink to="/mygroups">
+        <FT.StyledLink to="/group/list">
           <img src={groupIcon} alt="내 모임 아이콘" />
           <div>둘러보기</div>
         </FT.StyledLink>
@@ -26,9 +26,17 @@ function Footer() {
           <img src={likeIcon} alt="좋아요 아이콘" />
           <div>좋아요</div>
         </FT.StyledLink>
-        <FT.StyledLink to="/user/login">
+        <FT.StyledLink to="/login">
           <img src={userIcon} alt="로그인/가입 아이콘" />
           <div>로그인/가입</div>
+        </FT.StyledLink>
+        <FT.StyledLink to="/user/mypage">
+          <img src={userIcon} alt="로그인/가입 아이콘" />
+          <div>마이페이지</div>
+        </FT.StyledLink>
+        <FT.StyledLink to="/admin/user">
+          <img src={userIcon} alt="로그인/가입 아이콘" />
+          <div>관리자</div>
         </FT.StyledLink>
       </FT.MenuItem>
     </FT.StyledFooter>
--- src/routes/AdminRoutes.tsx
+++ src/routes/AdminRoutes.tsx
@@ -8,16 +8,51 @@ import InquiryList from '../pages/admin/InquiryList';
 import InquiryReply from '../pages/admin/InquiryReply';
 import AdminUser from '../pages/admin/AdminUser';
 
-function AdminRoutes () {
-    return (
-        <Routes>
-            <Route path="/admin/user" element={<Admin><AdminUser /></Admin>} />
-            <Route path="/admin/post" element={<Admin><AdminPost /></Admin>} />
-            <Route path="/admin/group" element={<Admin><AdminGroup /></Admin>} />
-            <Route path="/admin/inquirylist" element={<Admin><InquiryList /></Admin>} />
-            <Route path="/admin/inquiryreply" element={<InquiryReply />} />
-            <Route path="/admin" element={<Admin><></></Admin> } />
-        </Routes>
-    )
+function AdminRoutes() {
+  return (
+    <Routes>
+      <Route
+        path="/admin/user"
+        element={
+          <Admin>
+            <AdminUser />
+          </Admin>
+        }
+      />
+      <Route
+        path="/admin/post"
+        element={
+          <Admin>
+            <AdminPost />
+          </Admin>
+        }
+      />
+      <Route
+        path="/admin/group"
+        element={
+          <Admin>
+            <AdminGroup />
+          </Admin>
+        }
+      />
+      <Route
+        path="/admin/inquirylist"
+        element={
+          <Admin>
+            <InquiryList />
+          </Admin>
+        }
+      />
+      <Route path="/admin/inquiryreply" element={<InquiryReply />} />
+      <Route
+        path="/admin"
+        element={
+          <Admin>
+            <></>
+          </Admin>
+        }
+      />
+    </Routes>
+  );
 }
 export default AdminRoutes;
