bd8de8191e7140a8be7533426b270be01495b882
왕지은 <jieunwang00@gmail.com>
2023-08-29 10:20:41 +0900

    Add : 내가 작성한 글에 유저정보를 받아와서 프로필의 정보를 추가하였습니다.

--- src/pages/mypage/myposts/MyPosts.tsx
+++ src/pages/mypage/myposts/MyPosts.tsx
@@ -15,70 +15,128 @@ interface PostData {
   __v: number;
 }
 
-
 interface PostBoxProps {
   data?: PostData;
+}
+
+interface UserData {
+  name: any;
+  profilePic: string;
 }
 
 function MyPostsComponent({ data }: PostBoxProps) {
   const navigate = useNavigate();
   const [myPosts, setMyPosts] = useState<PostData[]>([]);
+  const [selectedPosts, setSelectedPosts] = useState<any[]>([]); // 추가: 선택된 포스트 정보를 저장할 상태
+  const [userData, setUserData] = useState<UserData | null>(null); // 추가: 유저 정보 상태
+  
+   useEffect(() => {
+     // 먼저 유저 정보를 가져옵니다.
+     const loginToken = getCookie('loginToken');
+
+     axios
+       .get('http://localhost:3001/api/v1/auth/me', {
+         headers: {
+           Authorization: `Bearer ${loginToken}`,
+         },
+         withCredentials: true,
+       })
+       .then(response => {
+         if (response.status === 200) {
+           setUserData(response.data.data.getUser);
+         } else {
+           navigate('/signup');
+         }
+       })
+       .catch(error => {
+         console.error('유저 정보 가져오기 에러:', error);
+         navigate('/');
+       });
+   }, []);
 
   useEffect(() => {
     // 먼저 유저 정보를 가져옵니다.
     const loginToken = getCookie('loginToken'); // getCookie 함수로 'loginToken' 쿠키 값을 가져옵니다.
-
-          axios
-            .get(`http://localhost:3001/api/v1/auth/me/posts`,
-            {
+    axios
+      .get(`http://localhost:3001/api/v1/auth/me/posts`, {
         headers: {
           Authorization: `Bearer ${loginToken}`,
         },
         withCredentials: true,
       })
-            .then(postsResponse => {
-              if (postsResponse.data.error === null) {
-                // 수정: 오류 확인
-                const userPosts: PostData[] = postsResponse.data.data;
-                setMyPosts(userPosts);
-              } else {
-                console.error(
-                  '게시글 가져오기 오류:',
-                  postsResponse.data.error,
+      .then(postsResponse => {
+        if (postsResponse.data.error === null) {
+          // 수정: 오류 확인
+          const userPosts: PostData[] = postsResponse.data.data;
+          setMyPosts(userPosts);
+          
+          // 모든 포스트를 가져오기 위한 함수
+          const fetchAllPosts = async () => {
+            for (const post of userPosts) {
+              try {
+                const postResponse = await axios.get(
+                  `http://localhost:3001/api/v1/group/${post.group_id}/posts/${post.post_id}`,
                 );
+
+                if (postResponse.data.error === null) {
+                  // 포스트 정보 처리
+                  const postData = {
+                    ...postResponse.data.data,
+                    // 여기에 원하는 추가 데이터를 저장할 수 있습니다.
+                  };
+                  setSelectedPosts(prevSelectedPosts => [
+                    ...prevSelectedPosts,
+                    postData,
+                  ]);
+                } else {
+                  console.error(
+                    '포스트 가져오기 오류:',
+                    postResponse.data.error,
+                  );
+                }
+              } catch (error) {
+                console.error('포스트 가져오기 에러:', error);
               }
-            })
-            .catch(error => {
-              console.error('게시글 가져오기 에러:', error);
-            });
-      
-  }, [navigate]);
+            }
+          };
+
+          // fetchAllPosts 함수 실행
+          fetchAllPosts();
+        } else {
+          console.error('게시글 가져오기 오류:', postsResponse.data.error);
+        }
+      })
+      .catch(error => {
+        console.error('게시글 가져오기 에러:', error);
+      });
+  }, []);
 
   return (
     <MyPostsStyle.Container>
       <MyPostsStyle.Wrapper>
-        {/* 좋아요한 모임 1개짜리 컴포넌트 */}
         <MyPostsStyle.GroupBoardList>
-          {myPosts.map(post => (
-            <MyPostsStyle.Boardbox key={post._id}>
+          {userData && selectedPosts.map(selectedPost => (
+            <MyPostsStyle.Boardbox key={selectedPost._id}>
               <MyPostsStyle.BoardLeft>
                 <MyPostsStyle.ProfileData>
                   <MyPostsStyle.ProfileImg
-                    src={post.createdAt}
-                    alt="유저 프로필사진"
+                    src={userData.profilePic}
+                    alt={`${userData.name}의 프로필 사진`}
                   />
                   <MyPostsStyle.UpdatedProfile>
-                    <MyPostsStyle.Writer>{post.user_id}</MyPostsStyle.Writer>
+                    <MyPostsStyle.Writer>{userData.name}</MyPostsStyle.Writer>
                     <MyPostsStyle.PostedDate>
-                      {post.createdAt}
+                      {selectedPost.createdAt}
                     </MyPostsStyle.PostedDate>
                   </MyPostsStyle.UpdatedProfile>
                 </MyPostsStyle.ProfileData>
 
-                <MyPostsStyle.Content>{post.post_id}</MyPostsStyle.Content>
+                <MyPostsStyle.Content>
+                  {selectedPost.content}
+                </MyPostsStyle.Content>
                 <MyPostsStyle.GroupDes>
                   <MyPostsStyle.ContentLikes>
-                    {post.__v}
+                    좋아요 수
                   </MyPostsStyle.ContentLikes>
                   <MyPostsStyle.ContentComment>
                     댓글 수
