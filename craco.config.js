f52c46e16e3ef0b0e129e83aa8718847895b8bf0
[SW5]최형욱 <guddnr888@gmail.com>
2023-09-01 02:57:20 +0900

    'fix: 배포를 위한 image.d.ts (이미지 타입정의) 파일생성"

--- craco.config.js
+++ craco.config.js
@@ -4,12 +4,30 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {},
-    configure: {
-      resolve: {
-        plugins: [
-          new TsconfigPathsPlugin({ configFile: path.resolve(__dirname, 'tsconfig.json') }),
-        ],
+    plugins: [
+      {
+        plugin: {
+          overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => {
+            cracoConfig.webpack.alias = {
+              ...cracoConfig.webpack.alias,
+              '@': path.resolve(__dirname, 'src'),
+            };
+            cracoConfig.webpack.configure = {
+              ...cracoConfig.webpack.configure,
+              resolve: {
+                ...cracoConfig.webpack.configure.resolve,
+                plugins: [
+                  ...(cracoConfig.webpack.configure.resolve.plugins || []),
+                  new TsconfigPathsPlugin({
+                    configFile: path.resolve(__dirname, 'tsconfig.json'),
+                  }),
+                ],
+              },
+            };
+            return cracoConfig;
+          },
+        },
       },
-    },
+    ],
   },
 };
