00a5ef574b1ddcc8de95b40744f57587d7d4b606
[SW5]최형욱 <guddnr888@gmail.com>
2023-09-01 03:01:44 +0900

    "fix: craco수정"

--- craco.config.js
+++ craco.config.js
@@ -1,33 +1,9 @@
-const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   webpack: {
-    alias: {},
-    plugins: [
-      {
-        plugin: {
-          overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => {
-            cracoConfig.webpack.alias = {
-              ...cracoConfig.webpack.alias,
-              '@': path.resolve(__dirname, 'src'),
-            };
-            cracoConfig.webpack.configure = {
-              ...cracoConfig.webpack.configure,
-              resolve: {
-                ...cracoConfig.webpack.configure.resolve,
-                plugins: [
-                  ...(cracoConfig.webpack.configure.resolve.plugins || []),
-                  new TsconfigPathsPlugin({
-                    configFile: path.resolve(__dirname, 'tsconfig.json'),
-                  }),
-                ],
-              },
-            };
-            return cracoConfig;
-          },
-        },
-      },
-    ],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 };
