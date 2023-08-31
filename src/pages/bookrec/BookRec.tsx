30a448edb38d5e6471e7b63f78ef2751429cfc27
[SW5] 김민태 <khsr98@ajou.ac.kr>
2023-08-31 21:51:28 +0900

    원상복귀

--- src/pages/bookrec/BookRec.tsx
+++ src/pages/bookrec/BookRec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import * as BC from '@/pages/BookRec/BookRec.Styled';
+import * as BC from '@/pages/bookRec/BookRec.Styled';
 import bookImg1 from '@/assets/img/bookImg/책추천12.png';
 import bookImg2 from '@/assets/img/bookImg/책추천13.png';
 import bookImg3 from '@/assets/img/bookImg/책추천3.png';
